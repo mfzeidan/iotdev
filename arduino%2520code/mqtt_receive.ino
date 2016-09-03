d6184b7b8b5b2c24b58261636e493d68c47b8b37
Mark Zeidan <mfzeidan@gmail.com>
2016-09-03 19:07:23 -0400

    Add files via upload
    pub and sub in same code

---
+++ arduino%2520code/mqtt_receive.ino
@@ -0,0 +1,81 @@
+#include <EEPROM.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+ 
+// Connect to the WiFi
+const char* ssid = "9a8fc8";
+const char* password = "278993750";
+const char* mqtt_server = "192.168.0.23";
+ 
+WiFiClient espClient; 
+PubSubClient client(espClient);
+ 
+const byte ledPin = 2;  // Pin with LED on Adafruit Huzzah
+ 
+void callback(char* topic, byte* payload, unsigned int length) {
+ Serial.print("Message arrived [");
+ Serial.print(topic);
+ Serial.print("] ");
+ for (int i=0;i<length;i++) {
+  char receivedChar = (char)payload[i];
+  Serial.print(receivedChar);
+  if (receivedChar == '0'){
+  
+  // ESP8266 Huzzah outputs are "reversed"
+  digitalWrite(ledPin, HIGH);
+  pubMQTT("mqtt_test","light is off");}
+  //Serial.println(" test");
+  
+  if (receivedChar == '1'){
+   digitalWrite(ledPin, LOW);
+   pubMQTT("mqtt_test","light is on");
+  }}
+  Serial.println();
+}
+ 
+ 
+void reconnect() {
+ // Loop until we're reconnected
+ while (!client.connected()) {
+ Serial.print("Attempting MQTT connection...");
+ // Attempt to connect
+ if (client.connect("ESP8266 Client")) {
+  Serial.println("connected");
+  // ... and subscribe to topic
+  client.subscribe("ledStatus");
+
+ } else {
+  Serial.print("failed, rc=");
+  Serial.print(client.state());
+  Serial.println(" try again in 5 seconds");
+  // Wait 5 seconds before retrying
+  delay(5000);
+  }
+ }
+}
+ 
+void setup()
+{
+ Serial.begin(9600);
+ 
+ client.setServer(mqtt_server, 1883);
+ client.setCallback(callback);
+ 
+ pinMode(ledPin, OUTPUT);
+}
+ 
+void loop()
+{
+ if (!client.connected()) {
+  reconnect();
+ }
+ client.loop();
+}
+
+void pubMQTT(String topic,String topic_val){
+    Serial.print("Newest topic " + topic + " value:");
+    Serial.println(String(topic_val).c_str());
+    client.publish(topic.c_str(), String(topic_val).c_str(), true);
+}
